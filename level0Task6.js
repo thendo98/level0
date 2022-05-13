e4a7b5959ceba93af12d42fd88ba7cccb4fda570
thendo98 <mufamadi.thrndo@gmail.com>
2022-05-13 23:10:21 +0200

    add level0Task6.js

---
+++ level0Task6.js
@@ -0,0 +1,11 @@
+function maximum(){
+    let max = arguments[0];
+    for(var i=1; i< arguments.length; i++){
+        if (arguments[i]>max){
+            max = arguments[i]
+        }
+    }
+    return max;
+}
+console.log(maximum(1,2,3) );
+console.log(maximum(1,22,3,2) );    
