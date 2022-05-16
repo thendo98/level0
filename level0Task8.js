7798c1a42b3247d4b64e8f14714728adbbc181ba
thendo98 <mufamadi.thrndo@gmail.com>
2022-05-16 21:56:37 +0200

    level0Task8.js

---
+++ level0Task8.js
@@ -0,0 +1,32 @@
+function numberToHourMinute(number){
+    var hour = 0;
+    var minutes = 0;
+
+    while (number > 0) {
+        if (number >= 60) {
+            number -= 60;
+            hour += 1;
+        }
+        else{
+            minutes = number;
+            break;
+        }
+    }
+    return pluralSingular(hour, minutes);
+}
+
+function pluralSingular(hour, minutes){
+    if (hour > 1 && minutes > 1 || hour == 0 && minutes == 0)
+        return `${hour} hours, ${minutes} minutes`;
+    if (hour <= 1 && minutes > 1)
+        return `${ hour} hour, ${minutes} minutes`;
+    if (hour > 1 && minutes <= 1)
+        return `${hour} hours, ${minutes} minute`;
+    return `${hour} hour, ${minutes} minute`;
+}
+
+console.log(numberToHourMinute(61));
+console.log(numberToHourMinute(71));
+console.log(numberToHourMinute(133));
+console.log(numberToHourMinute(0));
+console.log(numberToHourMinute(121));
