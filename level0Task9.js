bf71390f86e0c6ae4fba6f764d8c73d0e7a7ed3f
thendo98 <mufamadi.thrndo@gmail.com>
2022-05-17 07:00:45 +0200

    add task 9

---
+++ level0Task9.js
@@ -0,0 +1,27 @@
+function printVowels(word) {
+    word = word.toLowerCase();
+    let vowels = [];
+    for (i = 0; i < word.length; i++){
+        const letter = word[i];
+        if (isVowel(letter)){
+            if (!vowels.includes(letter))
+                vowels.push(letter);
+        }
+    }
+    console.log("Vowels: " + vowels.join(", "));
+
+}
+function isVowel(letter){
+    if (letter == 'a' || letter == 'A' ||
+    letter == 'e' || letter == 'E' ||
+    letter == 'i' || letter == 'I' ||
+    letter == 'o' || letter == 'O' ||
+    letter == 'u' || letter == 'U' 
+    ){
+        return true;
+    }else
+    return false
+
+}
+
+printVowels("Umuzi");
